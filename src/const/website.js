e1e3b6ad7d751b97699e3f8b1ce5bfcf86186fd0
金磊 <l.jin@civillan.com>
2018-10-17 09:19:11 +0800

    修改登录背景图，文字

--- src/const/website.js
+++ src/const/website.js
@@ -4,13 +4,13 @@ export default {
   author: "后台管理系统",
   whiteList: ["/login", "/404", "/401", "/lock"],
   info: {
-    title: "工程信息化智慧云平台管理系统",
+    title: "工程信息化智慧云平台后台管理系统",
     list: [
       'civillan.com 掌握智能科技·创领品质工程'
     ]
   },
   wel: {
-    title: '工程信息化智慧云平台管理系统',
+    title: '工程信息化智慧云平台后台管理系统',
     list: [
       'civillan.com 掌握智能科技·创领品质工程'
     ]
